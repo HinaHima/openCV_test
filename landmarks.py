b4516269bc38f793d7606f0aff1fa009c5b7394b
HimaHina <blankov.boris@mail.ru>
2022-08-13 20:35:14 +0800

    1

--- landmarks.py
+++ landmarks.py
@@ -2,11 +2,13 @@ import cv2
 import mediapipe as mp
 
 # Face mesh
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh()
 
 # Image
-image = cv2.imread("test.jpg")
+image = cv2.imread("check.jpg")
 height, width, _ = image.shape
 print(f"Height = {height}, widht = {width}")
 rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -15,12 +17,29 @@ rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 result = face_mesh.process(rgb_image)
 
 for facial_landmarks in result.multi_face_landmarks:
+    print(f"Face landmarks: {facial_landmarks}")
+    #mp_drawing.draw_landmarks(
+        #image=image,
+        #landmark_list=facial_landmarks,
+        #connections=mp_face_mesh.FACEMESH_TESSELATION,
+        #landmark_drawing_spec=None,
+        #connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style()
+    #)
+
+    vertices = []
+
     for i in range(0, 468):
         pt1 = facial_landmarks.landmark[i]
-        x = int(pt1.x * width)
-        y = int(pt1.y * height)
+        x = pt1.x
+        y = pt1.y
+        z = pt1.z
+        vertices.append((x, y, z),)
 
-        cv2.circle(image, (x, y), 2, (0, 0, 250), -1)
+    print(vertices)
 
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+        #cv2.circle(image, (x, y), 2, (0, 0, 250), -1)
+
+    #cv2.imwrite('meshed_image' + '.png', image )
+
+#cv2.imshow("Image", image)
+#cv2.waitKey(0)
